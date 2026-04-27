6936aa9433c66c26071b3f2985a89bb384f7cae3
kitik <svy1dan@gmail.com>
2026-04-27 18:43:03 +0300

    add new task

---
+++ middle level task/2181/2181. Merge Nodes in Between Zeros.go
@@ -0,0 +1,29 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeNodes(head *ListNode) *ListNode {
+	dummy := &ListNode{}
+	current := dummy
+	sum := 0
+	for head != nil {
+		if head.Val == 0 {
+			if sum != 0 {
+				current.Next = &ListNode{Val: sum}
+				current = current.Next
+				sum = 0
+			}
+		} else {
+			sum += head.Val
+		}
+		head = head.Next
+	}
+	return dummy.Next
+}
+
+func main() {
+
+}
