c62692cc81c3eb68fd5b7a4e7539139a668e7966
kitik <svy1dan@gmail.com>
2026-05-26 19:39:13 +0300

    added new task

---
+++ easy level tasks/263/263. Ugly Number.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func isUgly(n int) bool {
+    if n <= 0 {
+        return false
+    }
+
+    if n == 1 {
+        return true
+    }
+
+    for n%2 == 0 {
+        n /= 2
+    }
+
+    for n%3 == 0 {
+        n /= 3
+    }
+
+    for n%5 == 0 {
+        n /= 5
+    }
+
+    return n == 1
+}
+
+func main() {
+	fmt.Println(isUgly(6))
+}
