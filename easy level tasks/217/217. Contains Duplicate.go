49e9883e9a7c59d4e0d34635a88a7b6d48b02b6c
kitik <svy1dan@gmail.com>
2026-06-02 20:12:24 +0300

    added new tasl

---
+++ easy level tasks/217/217. Contains Duplicate.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func containsDuplicate(nums []int) bool {
+    seen := make(map[int]bool)
+
+    for _, num := range nums {
+        if seen[num] {
+            return true
+        }
+        seen[num] = true
+    }
+
+    return false
+}
+
+func main() {
+	fmt.Println(containsDuplicate([]int{1, 2, 3, 4}))
+}
